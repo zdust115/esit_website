b0f0929a81b53bc394d7d075c9c731700f11b7b2
zdust115 <rickyatomic911@gmail.com>
2025-04-28 17:24:19 +0200

    soluzione query

--- src/pages/api/sensors-data.js
+++ src/pages/api/sensors-data.js
@@ -1,11 +1,10 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({ region: process.env.AWS_REGION });
 const ddb = DynamoDBDocumentClient.from(client);
 
 export default async function handler(req, res) {
-  
   if (req.method === 'GET') {
     const { offset = 0, limit = 10 } = req.query;
     try {
@@ -15,28 +14,23 @@ export default async function handler(req, res) {
       console.error(error);
       res.status(500).json({ error: 'Errore nel recupero dei dati' });
     }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-
-  /* if (req.method === 'POST') {
-    const item = { userId: Date.now().toString(), ...req.body };
-    await ddb.send(new PutCommand({ TableName: 'SensorsData', Item: item }));
-    return res.status(201).json(item);
-  } */
- 
-  res.setHeader('Allow', ['GET', 'POST']);
-  res.status(405).end(`Method ${req.method} Not Allowed`);
 }
-
 
 async function getSensorData(offset, limit) {
   const params = {
     TableName: 'SensorsData',
-    Limit: limit,
-    ExclusiveStartKey: offset > 0 ? { id: offset.toString() } : undefined,
   };
 
   const command = new ScanCommand(params);
   const response = await ddb.send(command);
 
-  return response.Items || [];
+  // Simula l'offset e il limite
+  const items = response.Items || [];
+  const paginatedItems = items.slice(offset, offset + limit);
+
+  return paginatedItems;
 }
